d82e74361d02f8bb76a27967c127269ce7ca267b
rheyoampoyo <rheyoampoyo@gmail.com>
2024-10-29 22:37:58 -0400

    First run

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/rheyoampoyo/m09-demo',
+    author='Rheyan Ampoyo',
+    author_email='rheyoampoyo@gmail.com',
+    license='MIT',
+    packages=['demo']
+)
